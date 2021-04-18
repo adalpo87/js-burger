bfd37be817b1c381ad2b9910d4088ee35b1d466e
adalpo87 <adalpo87@gmail.com>
2021-04-18 22:04:47 +0200

    event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,43 @@
 /* ---------------- REFERENCES ------------------ */
-var price = document.getElementById('price')
-
-
+var price = document.getElementById('price');
+var btn = document.getElementById('btn');
+var user = document.getElementById('name');
+var ingredients = document.getElementsByClassName("ingredient-checkbox")
+//HTML COLLECTION
+console.log(ingredients);
 
 
 /* ---------------- SETTINGS ------------------ */
 // prezzo hamburger di default
 var pricedefault = 50;
+writePrice(pricedefault, price);
 
 
+/* ---------------- EVENT LISTENERS ------------------ */
+//aggancia un funzione che sarà chiamata quando accade un evento
+btn.addEventListener('click', function(){
+    //chiedere il nome 
+    var nomeInserito = user.value.trim();
+    console.log(nomeInserito.length);
+    if(nomeInserito === 0){
+        alert("Inserisci il nome");
+    }else{
+    //vedere quali sono stati checkati (input -> value è il prezzo)
+    //se checkati prendere il valore e sommarlo a quello originale
+     for (var i=0; i < ingredients.length; i++){
+        var ingrediente = ingredients[i];
+        
+     }
+    }
+    //verificare presenza coupon
+    //stampare il risultato 
+}) 
 
 
+/* ---------------- FUNCTIONS ------------------ */ 
+// devo creare una funzione che scriva il prezzo 
+function writePrice(valore, target){
+    target.innerHTML = valore.toFixed(2);
+  }
 
-/* ---------------- FUNCTIONS ------------------ */
-// devo creare una funzione che scriva il prezzo
-function wrightPrice(valore, tag){
-    
-}
+
