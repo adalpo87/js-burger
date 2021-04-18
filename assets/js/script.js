624c8310857d2c58ebf990a97f94f14c9f5d3e8a
adalpo87 <adalpo87@gmail.com>
2021-04-18 23:20:49 +0200

    quasi terminato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,60 @@
-/* ---------------- REFERENCES ------------------ */
-var price = document.getElementById('price');
-var btn = document.getElementById('btn');
-var user = document.getElementById('name');
-var ingredients = document.getElementsByClassName("ingredient-checkbox")
-//HTML COLLECTION
-console.log(ingredients);
-
-
-/* ---------------- SETTINGS ------------------ */
-// prezzo hamburger di default
-var pricedefault = 50;
-writePrice(pricedefault, price);
-
-
-/* ---------------- EVENT LISTENERS ------------------ */
-//aggancia un funzione che sarà chiamata quando accade un evento
-btn.addEventListener('click', function(){
-    //chiedere il nome 
-    var nomeInserito = user.value.trim();
-    console.log(nomeInserito.length);
-    if(nomeInserito === 0){
-        alert("Inserisci il nome");
-    }else{
-    //vedere quali sono stati checkati (input -> value è il prezzo)
-    //se checkati prendere il valore e sommarlo a quello originale
-     for (var i=0; i < ingredients.length; i++){
-        var ingrediente = ingredients[i];
-        
+/* -----------------REFERENCES----------------- */
+ var prezzo = document.getElementById('price');
+ var button = document.getElementById('button');
+ var user = document.getElementById('name');
+ var ingredients = document.getElementsByClassName("ingredient-checkbox");
+ var coupon = document.getElementById('coupon');
+ 
+/* -----------------SETTINGS----------------- */
+ var defaultPrice = 50;
+ var coupons = ['sconto2021', 'sconto-bool'];
+ var discount = 0.3;
+ writePrice(defaultPrice, prezzo);
+ 
+ /* -----------------EVENTS----------------- */
+ button.addEventListener('click', function(){
+ 
+   // controllo del nome
+   var nomeInserito = user.value.trim();
+ 
+   // verifico la presenza del nome
+   if(nomeInserito.length === 0){
+     alert("Inserisci il nome del burger");
+   }else{
+ 
+   // tramite un ciclo for vedo se l'elemento è checkato e allora lo sommo
+     var priceIngredient = 0;
+     for(var i = 0; i < ingredients.length; i++){
+       var ingredient = ingredients[i];
+       
+     // se checked è true allora lo inserisco
+       if(ingredient.checked === true){
+         priceIngredient += parseInt(ingredient.value);
+       }
+ 
      }
-    }
-    //verificare presenza coupon
-    //stampare il risultato 
-}) 
+     //sommo il prezzo di default e aggiungo gli ingredienti
+     var totalPrice = defaultPrice + priceIngredient;
+     
+     // controllo la presenza del coupon nell'array
+     if(coupons.includes(coupon.value) === true){
+       totalPrice -= totalPrice * discount;
+       //totalPrice *= (1 - discount);
+     }
+     
+      //PREZZO TOTALE
+     writePrice(totalPrice, prezzo);
+ 
+   }
+ 
+ });
+ 
+ 
+ 
+/* -----------------FUNZIONI----------------- */
 
-
-/* ---------------- FUNCTIONS ------------------ */ 
-// devo creare una funzione che scriva il prezzo 
-function writePrice(valore, target){
-    target.innerHTML = valore.toFixed(2);
-  }
-
-
+ 
+ // riceve il velare da scrivere e l'elemento dove deve esere scritto
+ function writePrice(value, target){
+   target.innerHTML = value.toFixed(2);
+ }
