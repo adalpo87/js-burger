5faed0a4c968b1b94c5aec992e9a62c93ba1fcdc
adalpo87 <adalpo87@gmail.com>
2021-04-18 20:04:25 +0200

    iniziato con struttura di base

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+/* ---------------- REFERENCES ------------------ */
+var price = document.getElementById('price')
+
+
+
+
+/* ---------------- SETTINGS ------------------ */
+// prezzo hamburger di default
+var pricedefault = 50;
+
+
+
+
+
+/* ---------------- FUNCTIONS ------------------ */
+// devo creare una funzione che scriva il prezzo
+function wrightPrice(valore, tag){
+    
+}
